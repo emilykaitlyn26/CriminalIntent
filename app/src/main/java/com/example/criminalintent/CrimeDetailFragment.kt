5d56090650df5d0561e6bb46dc1f57d11b3a9e70
Emily <emily.kania@uconn.edu>
2023-03-03 15:28:20 -0500

    Added Database

--- app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -1,15 +1,22 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
 import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
+import kotlinx.coroutines.launch
 import java.util.*
 
-class CrimeDetailFragment: Fragment() {
+class CrimeDetailFragment: androidx.fragment.app.Fragment() {
 
     private var _binding:FragmentCrimeDetailBinding? = null
 
@@ -18,17 +25,10 @@ class CrimeDetailFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private lateinit var crime: Crime
+    private val args: CrimeDetailFragmentArgs by navArgs()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        crime = Crime(
-            id = UUID.randomUUID(),
-            title = "",
-            date = Date(),
-            isSolved = false
-        )
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
     }
 
     override fun onCreateView(
@@ -44,16 +44,27 @@ class CrimeDetailFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply{
             crimeTitle.doOnTextChanged { text, _, _, _ ->
-                crime = crime.copy(title = text.toString())
+                crimeDetailViewModel.updateCrime { oldCrime->
+                    oldCrime.copy(title = text.toString())
+                }
             }
 
             crimeDate.apply{
-                text = crime.date.toString()
                 isEnabled = false
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-                crime = crime.copy(isSolved = isChecked)
+                crimeDetailViewModel.updateCrime { oldCrime->
+                    oldCrime.copy(isSolved = isChecked)
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                crimeDetailViewModel.crime.collect { crime->
+                    crime?.let{updateUi(it)}
+                }
             }
         }
     }
@@ -61,5 +72,15 @@ class CrimeDetailFragment: Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateUi(crime: Crime) {
+        binding.apply {
+            if (crimeTitle.text.toString() != crime.title) {
+                crimeTitle.setText(crime.title)
+            }
+            crimeDate.text = crime.date.toString()
+            crimeSolved.isChecked = crime.isSolved
+        }
     }
 }
---
+++ app/src/main/java/com/example/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,20 @@
+package com.example.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Update
+import com.example.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
+import java.util.*
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): Flow<List<Crime>>
+
+    @Query("SELECT * FROM crime where id=(:id)")
+    suspend fun getCrime(id: UUID): Crime
+
+    @Update
+    suspend fun updateCrime(crime: Crime)
+}
